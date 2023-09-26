b74d3ace089d8b2cd9113b0457e2c48217002380
MihaelaBahrim <mihaela.bahrim1@gmail.com>
2023-09-26 19:48:39 +0300

    first commit

---
+++ e2e/tests/main.js
@@ -0,0 +1,11 @@
+//main.js
+
+//Importam functia din alt fisier
+
+import { aduna } from "./math.js";
+
+//Folosim functia importata
+
+const suma = aduna(5,3);
+
+console.log(suma); //Afiseaza: 8
---
+++ e2e/tests/math.js
@@ -0,0 +1,7 @@
+//math.js
+
+//Functie pe care vrem sa o exportam
+
+export function aduna(a, b) {
+    return a + b;
+    }
